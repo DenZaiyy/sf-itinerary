137ba929bdb312744daf8efabd5c426f5375afb6
DenZaiyy <denzaiyy@outlook.fr>
2025-03-21 16:37:40 +0100

    feat: controllers for add new item

--- src/Controller/ItineraryController.php
+++ src/Controller/ItineraryController.php
@@ -2,33 +2,52 @@
 
 namespace App\Controller;
 
+use App\Form\ItineraryType;
 use App\Service\ApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ItineraryController extends AbstractController
 {
 
-    public function __construct(
-        private readonly ApiService $apiService
-    ){}
+    public function __construct(private readonly ApiService $apiService) {}
 
-    #[Route('/', name: 'itinerary_index')]
+    #[Route('/', name: 'itinerary_index', methods: ['GET'])]
     public function index(): Response
     {
-        $itineraries = $this->apiService->getItineraries();
         return $this->render('itinerary/index.html.twig', [
-            'itineraries' => $itineraries,
+            'itineraries' => $this->apiService->getItineraries(),
         ]);
     }
 
-    #[Route('/itinerary/{id}', name: 'itinerary_show')]
-    public function itinerary(string $id): Response
+    #[Route('/itinerary/{id}', name: 'itinerary_show', methods: ['GET'], priority: -1)]
+    public function show(string $id): Response
     {
-        $itinerary = $this->apiService->getItinerary($id);
         return $this->render('itinerary/show.html.twig', [
-            'itinerary' => $itinerary,
+            'itinerary' => $this->apiService->getItinerary($id),
+        ]);
+    }
+
+    #[Route('/itinerary/add', name: 'itinerary_new', methods: ['GET', 'POST'])]
+    public function new(Request $request): Response
+    {
+        $form = $this->createForm(ItineraryType::class);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash('success', 'Itinerary created successfully');
+            $data = $form->getData();
+
+            if($this->apiService->createItinerary($data)) {
+                return $this->redirectToRoute('itinerary_index');
+            }
+        }
+
+        return $this->render('itinerary/new.html.twig', [
+            "form" => $form->createView()
         ]);
     }
 }
---
+++ src/Controller/LocationController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\LocationType;
+use App\Service\ApiService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class LocationController extends AbstractController
+{
+
+    public function __construct(
+        private readonly ApiService $apiService,
+    ){}
+
+    #[Route('/location/add', name: 'location_new', methods: ["GET", "POST"])]
+    public function new(Request $request): Response
+    {
+        $form = $this->createForm(LocationType::class);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash('success', 'Location created successfully');
+            $data = $form->getData();
+            //dd($data);
+
+            if($this->apiService->createLocation($data)) {
+                return $this->redirectToRoute('itinerary_index');
+            }
+
+            dd($data);
+        }
+
+        return $this->render('location/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/location/add/{id}', name: 'location_new_itinerary', methods: ['GET'])]
+    public function addToItinerary(Request $request): Response
+    {
+        $form = $this->createForm(LocationType::class);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash('success', 'Location added to itinerary successfully');
+            $data = $form->getData();
+            dd($data);
+
+            if($this->apiService->addToItinerary($data)) {
+                return $this->redirectToRoute('location');
+            }
+
+            dd($data);
+        }
+
+        return $this->render('location/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
