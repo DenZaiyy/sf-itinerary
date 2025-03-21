3a485b474e548abf84fecffe39e33b52d048bcf4
DenZaiyy <denzaiyy@outlook.fr>
2025-03-21 11:26:49 +0100

    fix: rename home to itinerary

--- src/Controller/ItineraryController.php
+++ src/Controller/ItineraryController.php
@@ -7,29 +7,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class ItineraryController extends AbstractController
 {
 
     public function __construct(
-        private ApiService $apiService
-    )
-    {
-    }
+        private readonly ApiService $apiService
+    ){}
 
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'itinerary_index')]
     public function index(): Response
     {
         $itineraries = $this->apiService->getItineraries();
-        return $this->render('home/index.html.twig', [
+        return $this->render('itinerary/index.html.twig', [
             'itineraries' => $itineraries,
         ]);
     }
 
-    #[Route('/itinerary/{id}', name: 'app_itinerary')]
+    #[Route('/itinerary/{id}', name: 'itinerary_show')]
     public function itinerary(string $id): Response
     {
         $itinerary = $this->apiService->getItinerary($id);
-        return $this->render('home/show.html.twig', [
+        return $this->render('itinerary/show.html.twig', [
             'itinerary' => $itinerary,
         ]);
     }
