f4afa10057dbcc1c7b4c3acbcfa0e881fa9f89c8
DenZaiyy <denzaiyy@outlook.fr>
2025-04-07 14:16:48 +0200

    fix: update coords from selected value

--- assets/controllers/form-address_controller.js
+++ assets/controllers/form-address_controller.js
@@ -4,15 +4,34 @@ import {forEach} from "core-js/stable/dom-collections";
 export default class extends Controller {
     static targets = ['address', 'latitude', 'longitude']
     static debounceTimer = null
+    static selectCity = document.createElement('select')
 
     connect() {
         console.log("Stimulus connecté ✅");
         console.log("Address target :", this.addressTarget);
         console.log("Latitude target :", this.latitudeTarget);
         console.log("Longitude target :", this.longitudeTarget);
+        console.log('Select : ', this.constructor.selectCity)
+        const select = this.constructor.selectCity
+        select.id = "selectCity"
+
+        this.showSelect()
+
+        const parent = document.getElementById("address-field")
+
+        parent.appendChild(select)
+    }
+
+    showSelect() {
+        if(!this.addressTarget.value) {
+            this.constructor.selectCity.className = "hidden"
+        } else {
+            this.constructor.selectCity.className = "p-2 border border-gray-300 rounded-md w-full"
+        }
     }
 
     typing(event) {
+        this.showSelect()
         const address = this.addressTarget.value;
 
         // Nettoie l'ancien timer si l'utilisateur tape encore
@@ -27,7 +46,7 @@ export default class extends Controller {
     async fetchCoordinates(address) {
         if (!address) return;
 
-        const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(address)}`;
+        const url = `https://nominatim.openstreetmap.org/search?format=jsonv2&q=${encodeURIComponent(address)}&limit=20`;
 
         try {
             const response = await fetch(url, {
@@ -39,8 +58,21 @@ export default class extends Controller {
             const data = await response.json();
             console.log('Data: ', data)
 
+            const select = document.getElementById("selectCity")
+            select.innerHTML = ""
+
+
             if(data.length >= 0) {
-                const location = data[0];
+                for (const loc in data) {
+                    console.log('loc', loc)
+                    const option = document.createElement('option')
+                    option.value = loc
+                    option.innerText = data[loc]["display_name"]
+
+                    select.appendChild(option)
+                }
+
+                const location = data[select.value];
                 const lat = location.lat
                 const lon = location.lon
 
@@ -48,6 +80,18 @@ export default class extends Controller {
                 this.longitudeTarget.value = lon;
 
                 console.log('Coords updated: ', location.lat, location.lon)
+
+                select.addEventListener('change', (e) => {
+                    const dataValue = select.value
+                    const location = data[dataValue];
+                    const lat = location.lat
+                    const lon = location.lon
+
+                    this.latitudeTarget.value = lat;
+                    this.longitudeTarget.value = lon;
+
+                    console.log('Coords updated: ', location.lat, location.lon)
+                })
             } else {
                 console.warn('Aucune coordonnée trouvée pour cette adresse.')
             }
